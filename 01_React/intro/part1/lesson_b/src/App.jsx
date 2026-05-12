955a4098c392e2ec496812aec0fe2f570180d4c6
Марат И. Бирюшев <biryushev.m@corp.click.uz>
2026-05-12 20:00:53 +0500

    lesson b

---
+++ 01_React/intro/part1/lesson_b/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      <p>
+        {course.parts[0].name} {course.parts[0].exercises}
+      </p>
+    </div>
+  )
+}
+export default App
